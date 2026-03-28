dca79cd0c15d7fa13336e2088b98240e68e8028f
Niladri-Gudu <niladrigudu@gmail.com>
2026-03-29 00:00:46 +0530

    updated contract

--- packages/contracts/contracts/SmartWallet.sol
+++ packages/contracts/contracts/SmartWallet.sol
@@ -26,7 +26,14 @@ contract SmartWallet {
     struct UserOperation {
         address sender;
         uint256 nonce;
+        bytes initCode;
         bytes callData;
+        uint256 callGasLimit;
+        uint256 verificationGasLimit;
+        uint256 preVerificationGas;
+        uint256 maxFeePerGas;
+        uint256 maxPriorityFeePerGas;
+        bytes paymasterAndData;
         bytes signature;
     }
 
