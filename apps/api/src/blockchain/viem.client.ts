b483c5a4bf2890302947307a13f5502c61af98e7
Niladri-Gudu <niladrigudu@gmail.com>
2026-04-04 16:47:55 +0530

    redepolyed contracts using remix ide

--- apps/api/src/blockchain/viem.client.ts
+++ apps/api/src/blockchain/viem.client.ts
@@ -1,20 +1,20 @@
-import 'dotenv/config';
-import { createPublicClient, createWalletClient, http } from 'viem';
-import { privateKeyToAccount } from 'viem/accounts';
-import { sepolia } from 'viem/chains';
+// import 'dotenv/config';
+// import { createPublicClient, createWalletClient, http } from 'viem';
+// import { privateKeyToAccount } from 'viem/accounts';
+// import { sepolia } from 'viem/chains';
 
-const RPC_URL = process.env.RPC_URL;
-const PRIVATE_KEY = process.env.PRIVATE_KEY as `0x${string}`;
+// const RPC_URL = process.env.RPC_URL;
+// const PRIVATE_KEY = process.env.PRIVATE_KEY as `0x${string}`;
 
-export const publicClient = createPublicClient({
-  chain: sepolia,
-  transport: http(RPC_URL),
-});
+// export const publicClient = createPublicClient({
+//   chain: sepolia,
+//   transport: http(RPC_URL),
+// });
 
-const account = privateKeyToAccount(PRIVATE_KEY);
+// const account = privateKeyToAccount(PRIVATE_KEY);
 
-export const walletClient = createWalletClient({
-  account,
-  chain: sepolia,
-  transport: http(RPC_URL),
-});
+// export const walletClient = createWalletClient({
+//   account,
+//   chain: sepolia,
+//   transport: http(RPC_URL),
+// });
